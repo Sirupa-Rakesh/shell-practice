57eeedcd15c2c04c04b2a586322b41cb0919ceec
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-01-29 15:43:25 +0530

    changes

--- datatypes.sh
+++ datatypes.sh
@@ -8,4 +8,6 @@ NUMBER=255
 # -ne -> not equal
 if [ $NUMBER -gt 20 ]; then 
     echo "given number is : $NUMBER is grater than 20"
+else
+    echo "given number is : $NUMBER is not grater than 20"
 fi
