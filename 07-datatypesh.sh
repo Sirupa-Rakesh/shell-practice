800d985e6d4042da7f579dc5f66d7e6d548860e7
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-01-29 15:29:24 +0530

    rakesh

---
+++ 07-datatypesh.sh
@@ -0,0 +1,12 @@
+#!  bin/bash
+NUMBER=255
+# -gt -> grater than
+# -nt -> not grater than
+# -eq -> equal
+# -ne -> not equal
+if [ $NUMBER -gt 20 ]; then 
+echo "given number is grater than 20"
+else
+echo "given number is not grater than 20"
+fi
+
