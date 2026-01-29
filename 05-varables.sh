ed967091268c6e3f7ced301e394b72ad47726d7f
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-01-29 09:07:04 +0530

    actions

--- 05-varables.sh
+++ 05-varables.sh
@@ -3,8 +3,8 @@
 echo " all arg passed to script : $@"
 echo "number of arg passed to script : $#"
 echo "script name : $0"
-ech0 "present working directory : $PWD"
-ecjo " who is running the script : $USER"
+echo "present working directory : $PWD"
+echo " who is running the script : $USER"
 echo " home directory of user : $HOME"
 echo "process id of this script : $$"
 sleep 100 &
