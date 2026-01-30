e0152fe79a451b1cf8e970ce9530bf885aed8c74
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-01-30 08:30:39 +0530

    changes

--- 10-logs.sh
+++ 10-logs.sh
@@ -2,29 +2,32 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-scripts"
-LOGS_FILE="/var/log/shell-scripts/$0.log"
+SCRIPT_NAME=$(basename "$0")
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-
+# Check root user
 if [ $USERID -ne 0 ]; then
     echo "Please run this script as root user"
     exit 1
 fi
 
-VALIDATE ()
-{
+# Create logs directory
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
     if [ $1 -ne 0 ]; then
-        echo "$2 .... FAILURED"
+        echo "$2 .... FAILED"
         exit 1
     else
         echo "$2 .... SUCCESS"
     fi
 }
 
-dnf install nginx -y $>> $LOGS_FILE 
-VALIDATE $? "installing nginx"
+dnf install nginx -y >> $LOGS_FILE 2>&1
+VALIDATE $? "Installing nginx"
 
-dnf install mysql -y  $>> $LOGS_FILE
-VALIDATE $? "installing mysql"
+dnf install mysql -y >> $LOGS_FILE 2>&1
+VALIDATE $? "Installing mysql"
 
-dnf install nodejs -y  $>> $LOGS_FILE
-VALIDATE $? "installing nodejs"
+dnf install nodejs -y >> $LOGS_FILE 2>&1
+VALIDATE $? "Installing nodejs"
