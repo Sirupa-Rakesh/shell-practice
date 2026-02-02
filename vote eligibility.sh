6f67b3729c027d9584a9ec58fb11eb7892b67dd9
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-02-02 18:33:04 +0530

    actions

---
+++ vote eligibility.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# to check voting eligibility
+
+echo "enter your age "
+if [$age -ge 18]; then
+   echo "you are eligible to vote"
+else
+    echo "you are not eligible to vote" 
+
+fi
