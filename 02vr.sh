76ec5fa8eda2e63d999071f5c6ca25b11d7bd522
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-02-12 10:49:04 +0530

    actions

---
+++ 02vr.sh
@@ -0,0 +1,20 @@
+#! bin/bash
+
+# varables
+
+name="sandhya"
+class="INTER 2ND YEAR"
+age="21"
+
+
+
+
+echo "my name is $name"
+echo "my class is $class"
+echo "my age is $age"
+
+name ="sandy"
+echo "my name is $name"
+
+HOSTNAME="$HOSTNAME"
+echo "my hostname is $HOSTNAME"
